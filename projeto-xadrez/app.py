35fa4cefec2a628263150708929a5f9a460b7df2
Adrian Morales <adrian.mflima@gmail.com>
2026-06-04 12:17:06 -0300

    Inicio da integracao do Chessboard

--- projeto-xadrez/app.py
+++ projeto-xadrez/app.py
@@ -2,10 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-    return render_template('index.html')
+@app.route("/aprender")
+def aprender():
+    return render_template("aprender.html")
+
+@app.route("/exercicios")
+def exercicios():
+    return render_template("exercicios.html")
+
+@app.route("/perfil")
+def perfil():
+    return render_template("perfil.html")
 
 
 if __name__ == '__main__':
